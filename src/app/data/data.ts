c03a141ec0674e0d1484888a1762a626c4abddb1
carlos_eduardo <ceduardoeab10@gmail.com>
2023-10-08 12:19:31 -0300

    adding data

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -43,6 +43,33 @@ export const data = [
         gameType: "Digital PS4 | Disc",
         gamePrice: "R$ 219,99",
         gameText: "Lorem ipsum dolor sit amet. Qui veritatis necessitatibus sed voluptatem laudantium sit officia quam. Ad voluptatum nihil sit voluptate distinctio non placeat praesentium qui iure consequatur et dolor sunt quo ratione omnis et sequi dolorem. Et laborum omnis et voluptatibus veritatis et omnis tenetur! Nam numquam earum sit expedita rerum eos recusandae voluptas et corrupti nihil est provident labore sit molestiae sunt."
-    }
+    },
+    {
+        gameName: "Spider Man",
+        id: "6",
+        gameCover: "https://upload.wikimedia.org/wikipedia/pt/7/78/Spider-Man_jogo_2018_capa.png",
+        gameLabel: "exclusive",
+        gameType: "Digital PS4 | Disc",
+        gamePrice: "R$ 229,99",
+        gameText: "Lorem ipsum dolor sit amet. Qui veritatis necessitatibus sed voluptatem laudantium sit officia quam. Ad voluptatum nihil sit voluptate distinctio non placeat praesentium qui iure consequatur et dolor sunt quo ratione omnis et sequi dolorem. Et laborum omnis et voluptatibus veritatis et omnis tenetur! Nam numquam earum sit expedita rerum eos recusandae voluptas et corrupti nihil est provident labore sit molestiae sunt."
+    },
+    {
+        gameName: "The last of us",
+        id: "7",
+        gameCover: "https://static.posters.cz/image/750/posters/the-last-of-us-key-art-i127761.jpg",
+        gameLabel: "exclusive",
+        gameType: "Digital PS4 | Disc",
+        gamePrice: "R$ 319,99",
+        gameText: "Lorem ipsum dolor sit amet. Qui veritatis necessitatibus sed voluptatem laudantium sit officia quam. Ad voluptatum nihil sit voluptate distinctio non placeat praesentium qui iure consequatur et dolor sunt quo ratione omnis et sequi dolorem. Et laborum omnis et voluptatibus veritatis et omnis tenetur! Nam numquam earum sit expedita rerum eos recusandae voluptas et corrupti nihil est provident labore sit molestiae sunt."
+    },
+    {
+        gameName: "Dark souls",
+        id: "8",
+        gameCover: "https://m.media-amazon.com/images/I/71bN7mqVC+L._AC_UF1000,1000_QL80_.jpg",
+        gameLabel: "NEW",
+        gameType: "Digital PS4 | Disc",
+        gamePrice: "R$ 229,99",
+        gameText: "Lorem ipsum dolor sit amet. Qui veritatis necessitatibus sed voluptatem laudantium sit officia quam. Ad voluptatum nihil sit voluptate distinctio non placeat praesentium qui iure consequatur et dolor sunt quo ratione omnis et sequi dolorem. Et laborum omnis et voluptatibus veritatis et omnis tenetur! Nam numquam earum sit expedita rerum eos recusandae voluptas et corrupti nihil est provident labore sit molestiae sunt."
+    },
 
 ]
